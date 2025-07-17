39e91fe18360a03a2f531342cb4b65c73bd3ea2e
SathisKumar <sathism1701@gmail.com>
2025-07-17 21:55:02 +0530

    Find luckiest numerber number of lines reduced

--- FindLuckyNumber.js
+++ FindLuckyNumber.js
@@ -1,26 +1,53 @@
-function findLuckyNumber(nums){
+function findLuckyNumber(nums) {
     const pareIndex = {};
-    for(let i = 0; i < nums.length;i ++){
+    let maxLucky = -1;
+
+
+    for (let i = 0; i < nums.length; i++) {
         const value = nums[i];
-        if(value in pareIndex){
+        if (value in pareIndex) {
             pareIndex[value]++;
-        }else{
-           pareIndex[value] = 1;
-        }  
+        } else {
+            pareIndex[value] = 1;
+        }
 
     }
-let maxLucky = -1;
-  for (let name in pareIndex) {
-      let key = Number(name);
-      let frequency = pareIndex[name]
-      if(key === frequency){
-          if(key > maxLucky){
-              maxLucky = key
-          }
-      }
-  }
-    
+
+
+    for (let name in pareIndex) {
+        let key = Number(name);
+        let frequency = pareIndex[name]
+        if (key === frequency) {
+            if (key > maxLucky) {
+                maxLucky = key
+            }
+        }
+    }
+
     return maxLucky;
 }
 
-console.log(findLuckyNumber([2,2,3,3,3,4,4,4,4]));
+// console.log(findLuckyNumber([2, 2, 3, 3, 3, 4, 4, 4, 4]));
+
+//code optimized
+
+function findLuckyNumber(nums) {
+    const pareIndex = {};
+    let maxLucky = -1;
+
+
+    for (let i = 0; i < nums.length; i++) {
+        const value = nums[i];
+        pareIndex[value] = (pareIndex[value] || 0) + 1
+    }
+
+    for (let name in pareIndex) {
+        if (Number(name) === pareIndex[name] && Number(name) > maxLucky) {
+            maxLucky = Number(name)
+        }
+    }
+
+    return maxLucky;
+}
+
+console.log(findLuckyNumber([2, 2, 3, 3, 3, 4, 4, 4, 4]));
